0ca81ff90fa9a2954c411b5fbcef485f5aa1cbcf
lightsource <142080508+l1ghtsource@users.noreply.github.com>
2024-01-12 15:53:43 +0300

    Update just-a-name-generator-for-beats.py

--- just-a-name-generator-for-beats.py
+++ just-a-name-generator-for-beats.py
@@ -9,19 +9,19 @@ from deep_translator import GoogleTranslator
 import tkinter as tk
 from tkinter import ttk
 
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load('en_core_web_sm')
 
 
 def extract_random_adjective_noun(text):
     doc = nlp(text)
 
-    adjectives = [token.text for token in doc if token.pos_ == "ADJ"]
-    nouns = [token.text for token in doc if token.pos_ == "NOUN"]
+    adjectives = [token.text for token in doc if token.pos_ == 'ADJ']
+    nouns = [token.text for token in doc if token.pos_ == 'NOUN']
 
     if adjectives and nouns:
         random_adjective = random.choice(adjectives)
         random_noun = random.choice(nouns)
-        return f"{random_adjective} {random_noun}"
+        return f'{random_adjective} {random_noun}'
     else:
         return None
 
@@ -43,26 +43,26 @@ def fetch_and_translate():
             if random_phrase:
                 translator = GoogleTranslator(source='auto', target='ru')
                 translated_phrase = translator.translate(random_phrase)
-                result_text.insert(tk.END, translated_phrase + "\n\n")
+                result_text.insert(tk.END, translated_phrase + '\n')
             else:
-                result_text.insert(tk.END, "Не удалось найти подходящее словосочетание.\n\n")
+                result_text.insert(tk.END, 'Не удалось найти подходящее словосочетание\n')
     else:
-        result_text.insert(tk.END, "Не удалось загрузить страницу.")
+        result_text.insert(tk.END, 'Не удалось загрузить страницу')
 
 
 root = tk.Tk()
-root.title("Average Lovecraft Enjoyer")
+root.title('Average Lovecraft Enjoyer')
 
 style = ttk.Style()
-style.configure("TButton", padding=(5, 5, 12, 12))
+style.configure('TButton', padding=(5, 5, 12, 12))
 
-url_label = ttk.Label(root, text="Введите URL:")
-url_label.grid(column=0, row=0, columnspan=2, padx=10, pady=10, sticky="w")
+url_label = ttk.Label(root, text='Введите URL:')
+url_label.grid(column=0, row=0, columnspan=2, padx=10, pady=10, sticky='w')
 
 url_entry = ttk.Entry(root, width=50)
-url_entry.grid(column=2, row=0, padx=10, pady=10, sticky="w")
+url_entry.grid(column=2, row=0, padx=10, pady=10, sticky='w')
 
-fetch_button = ttk.Button(root, text="Just generate...", command=fetch_and_translate)
+fetch_button = ttk.Button(root, text='Just generate...', command=fetch_and_translate)
 fetch_button.grid(column=0, row=1, columnspan=3, padx=10, pady=10)
 
 result_text = tk.Text(root, height=10, width=50)
